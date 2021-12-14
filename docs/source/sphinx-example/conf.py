ecba5ef527e1a69cf0f0075e994df9d4711523cb
Joachim Holwech <joachim.holwech@gmail.com>
2021-12-14 08:41:54 +0100

    Formatting

--- docs/source/sphinx-example/conf.py
+++ docs/source/sphinx-example/conf.py
@@ -17,9 +17,11 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'az-static-web-app-docs-template'
-copyright = '2020, Author'
-author = 'Author'
+from typing import List
+
+project = "az-static-web-app-docs-template"
+copyright = "2020, Author"
+author = "Author"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,16 +29,15 @@ author = 'Author'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions: List[str] = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns: List[str] = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -44,9 +45,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
