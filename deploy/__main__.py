f841812b645ac73ecd07d681cd639a2d541b1792
Joachim Holwech <joachim.holwech@gmail.com>
2021-12-14 11:02:56 +0100

    Disable workflow generation in IaS

--- deploy/__main__.py
+++ deploy/__main__.py
@@ -12,6 +12,8 @@ static_site = azure_native.web.StaticSite(
         api_location="api",
         app_artifact_location="",
         app_location="docs/build",
+        github_action_secret_name_override="DEPLOY_TOKEN",
+        skip_github_action_workflow_generation=True
     ),
     location="West Europe",
     name="az-static-web-app-docs-template",
