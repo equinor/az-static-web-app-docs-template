171ce2d98e2e4363f60e74aa5a8f358bccc5ae2b
Viggo tellefsen Wivestad <viggotw@gmail.com>
2022-02-23 12:39:00 +0100

    Minor changes to satisfy the Black code formatter

--- docs/source/sphinx-example/conf.py
+++ docs/source/sphinx-example/conf.py
@@ -29,7 +29,7 @@ author = "Author"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions: List[str] = ['myst_parser']
+extensions: List[str] = ["myst_parser"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
