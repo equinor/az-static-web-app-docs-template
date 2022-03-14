5c7f519f4c4459bf14062dd24ed6a3ced20c4a8e
Viggo Wivestad <viggotw@gmail.com>
2022-03-14 13:40:20 +0100

    Add role assignment app

---
+++ api/GetRoles/index.js
@@ -0,0 +1,41 @@
+const fetch = require('node-fetch').default;
+
+// add role names to this object to map them to group ids in your AAD tenant
+const roleGroupMappings = {
+    // 'admin': '21a96550-aa02-486e-9297-e6e51b6398fc',
+    'reader': 'fd4d69ee-fc70-4b05-9a91-efa1dfae99f2'
+};
+
+module.exports = async function (context, req) {
+    const user = req.body || {};
+    const roles = [];
+    
+    for (const [role, groupId] of Object.entries(roleGroupMappings)) {
+        if (await isUserInGroup(groupId, user.accessToken)) {
+            roles.push(role);
+        }
+    }
+
+    context.res.json({
+        roles
+    });
+}
+
+async function isUserInGroup(groupId, bearerToken) {
+    const url = new URL('https://graph.microsoft.com/v1.0/me/memberOf');
+    url.searchParams.append('$filter', `id eq '${groupId}'`);
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${bearerToken}`
+        },
+    });
+
+    if (response.status !== 200) {
+        return false;
+    }
+
+    const graphResponse = await response.json();
+    const matchingGroups = graphResponse.value.filter(group => group.id === groupId);
+    return matchingGroups.length > 0;
+}
