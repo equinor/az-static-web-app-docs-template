e2c041cbf4b48c4dac184beff3ffde69441e3982
Viggo T. Wivestad <VTW@equinor.com>
2022-06-13 12:02:27 +0200

    Add solution from mss-sdlc-web-app

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -1,46 +1,96 @@
-const fetch = require('node-fetch').default;
+let appInsights = require('applicationinsights');
+const crypto = require('crypto');
 
-// add role names to this object to map them to group ids in your AAD tenant
-const roleGroupMappings = {
-    // '<custom_role_name>': '<AAD_GROUP_OBJECT_ID>',
-    'reader': '7bd6f561-18f8-4cd2-b3a7-75b9c8c4c613'
-};
+
+// Environment variable APPLICATIONINSIGHTS_CONNECTION_STRING or APPINSIGHTS_INSTRUMENTATIONKEY required
+appInsights.setup()
+let client = appInsights.defaultClient;
 
 module.exports = async function (context, req) {
-    const user = req.body || {};
-    const roles = [];
-    
-    for (const [role, groupId] of Object.entries(roleGroupMappings)) {
-        if (await isUserInGroup(groupId, user.accessToken)) {
-            roles.push(role);
+    const requestId = crypto.randomBytes(16).toString('hex'); // Not super unique, but unique enough
+    try {
+
+        let body = req.body;
+
+        client.trackTrace({
+            message: `Request started`,
+            properties: { requestId: requestId, messageTemplate: 'Request started', payloadType: (typeof body) }
+        });
+
+        // Sort of horrible workaround on Azure bug. Can be removed when resolved
+        if ((typeof body) === "string"){
+            let patchedBody = `${body}"}`;
+            let parsedOk = false;
+            try {
+                patchedBody = JSON.parse(patchedBody);
+                parsedOk = true;
+            }
+            catch{}
+
+            if (parsedOk && (typeof(patchedBody) === 'object')){
+                client.trackTrace({
+                    message: `Patched malformed json`,
+                    properties: { requestId: requestId, messageTemplate: 'Patched malformed json' }
+                });
+                
+                body = patchedBody;
+            }
+            else {
+                client.trackException({
+                    exception: new Error("Malformed userinfo and patching failed"),
+                    properties: { requestId: requestId, payload: body, messageTemplate: 'Malformed userinfo and patching failed' }
+                });
+                return context.res.status(400).json({
+                    "error": "malformed request"
+                })
+            }
+        }
+
+        const user = body || {};
+
+        if (user.accessToken) {
+            // Try to avoid logging this
+            user.accessToken = "Removed";
+        }
+
+        const roles = [];
+
+        if (!user.claims) {
+            client.trackException({
+                exception: new Error(`Payload has no claims`),
+                properties: { requestId: requestId, payload: user, messageTemplate: 'Payload has no claims' }
+            });
+
+            return context.res.status(400).json({
+                "error": "malformed request"
+            })
+        }
+        else {
+            user.claims.forEach(claim => {
+                if (claim.typ === "http://schemas.microsoft.com/ws/2008/06/identity/claims/role" || claim.typ === "roles") {
+                    // Mapped from role claims (aad -> enterprise application -> users&groups)
+                    roles.push(claim.val);
+                }
+            });
+
+            client.trackTrace({
+                message: `User ${user.userId} resolved with ${roles}`,
+                properties: { requestId: requestId, userOid: user.userId, userRoles: roles, messageTemplate: 'User {userOid} resolved with {userRoles}' }
+            });
+
+            return context.res.status(200).json({
+                roles
+            });
         }
     }
+    catch (e) {
+        client.trackException({
+            exception: new Error(`Request failed with ${e}`),
+            properties: { requestId: requestId, error: e, messageTemplate: 'Request failed with {error}' }
+        });
 
-    context.res.json({
-        roles
-    });
-}
-
-async function isUserInGroup(groupId, bearerToken) {
-    const url = new URL(`https://graph.microsoft.com/v1.0/me/memberOf/${groupId}`);
-    // url.searchParams.append('$filter', `id eq '${groupId}'`);
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${bearerToken}`
-        },
-    });
-
-    await response.json();
-
-    if (response.status == 200) {
-        return true;
+        return context.res.status(500).json({
+            "error": "Unexpected error"
+        })
     }
-    else {
-        return false;
-    }
-
-    // const graphResponse = await response.json();
-    // const matchingGroups = graphResponse.value.filter(group => group.id === groupId);
-    // return matchingGroups.length > 0;
 }
